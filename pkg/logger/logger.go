03727395600f599022386774fb839a113d047030
SenSeTioN <lankovedik@gmail.com>
2025-03-07 18:40:54 +0300

    refactor: delete if else

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -11,15 +11,16 @@ func NewLogger(config *config.LogConfig) *slog.Logger {
 	var logger *slog.Logger
 	level := slog.Level(config.Level)
 
-	if config.Format == "json" {
+	switch config.Format {
+	case "json":
 		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 			Level: level,
 		}))
-	} else {
+
+	default:
 		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 			Level: level,
 		}))
-
 	}
 
 	return logger
