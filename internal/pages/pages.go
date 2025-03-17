ebdd79763c76e3ca2a6683af14492f4a0ecc96ac
SenSeTioN <lankovedik@gmail.com>
2025-03-17 21:54:37 +0300

    refactor: delete logger

--- internal/pages/pages.go
+++ internal/pages/pages.go
@@ -1,15 +1,14 @@
 package pages
 
 import (
-	"log/slog"
 	"sensetion/go-fiber/internal/pages/home"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupRoutes(app *fiber.App, logger *slog.Logger) {
+func SetupRoutes(app *fiber.App) {
 	// v := app.Group("/v1")
 	// api := v.Group("/api")
 
-	home.NewHandler(app, logger)
+	home.NewHandler(app)
 }
